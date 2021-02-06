bdeb888f19ca3d6dc70b429ef319be7bf436fc74
noofreuuuh <dev.n2ofr@gmail.com>
2021-02-06 23:32:49 +0000

    improve init of NeoVim

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 # Create link to NeoVim config file
-mkdir ${HOME}/.config/nvim
+[ ! -d ${HOME}/.config ] && mkdir ${HOME}/.config
+[ ! -d ${HOME}/.config/nvim ] && mkdir ${HOME}/.config/nvim
+[ -d ${HOME}/.config/nvim/plugged ] && rm -Rf ${HOME}/.config/nvim/plugged
+[ -f ${HOME}/.config/nvim/init.vim ] && rm ${HOME}/.config/nvim/init.vim
 ln -s ${PWD}/nvim/init.vim ${HOME}/.config/nvim/init.vim
