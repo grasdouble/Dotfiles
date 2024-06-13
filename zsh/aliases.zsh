1b2c8ae3883db3cf68e7473b89ddc0f222e83df8
Noofreuuuh <dev.n2ofr@gmail.com>
2024-06-13 10:39:30 +0200

    add git-prune-branches in aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -33,3 +33,14 @@ alias setCalibreTmp='code ~/Library/Preferences/calibre/macos-env.txt'
 
 # list of all aliases
 alias lll='echo "brewcask, setCalibreTmp, ttop, fshow, fhide, cleanupDS, kdock, addDockSeparator, zipFolders"'
+
+function git-prune-branches() {
+        echo "switching to master or main branch.."
+        git branch | grep 'main\|master' | xargs -n 1 git checkout
+        echo "fetching with -p option...";
+        git fetch -p;
+        echo "running pruning of local branches"
+        git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -d ;
+}
+
+alias gpb='git-prune-branches'
